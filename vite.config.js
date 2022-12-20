1ce9bb94cf8886f0b6c13779013f32974e3e6b50
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2022-12-20 17:25:28 +0000

    Fixed relative paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<dynamic-react-form>/",
+  base: "/dynamic-react-form/",
   plugins: [react()],
 });
